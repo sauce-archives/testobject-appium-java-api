88a5cedd36c5d2da62ccbabf88fc321094c529ec
Malcolm Crum <mcrum@testobject.com>
2016-01-04 13:44:33 +0100

    Use HTTP proxy for rest client if available. Checks for http.proxyServer, http.proxyPort, http.proxyUser, and httpProxyPassword environment variables, and if they are available uses them when configuring the Apache client.

--- src/main/java/org/testobject/appium/internal/RestClient.java
+++ src/main/java/org/testobject/appium/internal/RestClient.java
@@ -7,6 +7,8 @@ import com.sun.jersey.api.client.filter.LoggingFilter;
 import com.sun.jersey.client.apache.ApacheHttpClient;
 import com.sun.jersey.client.apache.config.ApacheHttpClientConfig;
 import com.sun.jersey.client.apache.config.DefaultApacheHttpClientConfig;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 
 import java.io.Closeable;
 
@@ -18,6 +20,11 @@ public class RestClient implements Closeable {
             ApacheHttpClientConfig config = new DefaultApacheHttpClientConfig();
             config.getProperties().put(ApacheHttpClientConfig.PROPERTY_HANDLE_COOKIES, true);
 
+            boolean useProxy = System.getenv("http.proxyServer") != null;
+            if (useProxy) {
+                addProxyConfiguration(config);
+            }
+
             Client client = ApacheHttpClient.create(config);
             client.addFilter(new LoggingFilter(System.out));
             client.addFilter(new HTTPBasicAuthFilter(token, ""));
@@ -25,6 +32,24 @@ public class RestClient implements Closeable {
             WebResource baseResource = client.resource(baseUrl + "/rest/appium/v1/");
 
             return new RestClient(client, baseResource);
+        }
+
+        private static void addProxyConfiguration(ApacheHttpClientConfig config) {
+            String server = System.getenv("http.proxyServer");
+            String port = System.getenv("http.proxyPort");
+            String url;
+            if (port == null) {
+                url = "http://" + server;
+            } else {
+                url = "http://" + server + ":" + port;
+            }
+            config.getProperties().put(DefaultApacheHttpClientConfig.PROPERTY_PROXY_URI, url);
+
+            String username = System.getenv("http.proxyUser");
+            String password = System.getenv("http.proxyPassword");
+            if (username != null && password != null) {
+                config.getState().getHttpState().setProxyCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+            }
         }
 
     }
